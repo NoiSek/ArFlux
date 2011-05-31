5ebf132783b8cc636d60c38a47c44a40cdaf2aa3
unknown <Administrator@.(none)>
2011-05-31 18:56:21 -0400

    Barebones, go!

---
+++ application/logs/2011/05/30.php
@@ -0,0 +1,3 @@
+<?php defined('SYSPATH') or die('No direct script access.'); ?>
+
+2011-05-30 16:36:28 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: game/kohana/index.php ~ SYSPATH/classes/kohana/request.php [ 760 ]
