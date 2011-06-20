33ef5724026757eec215c60bb27564d62cd7ed3c
Noi Sek <Cptn.Lightning@gmail.com>
2011-06-20 13:51:49 -0400

    Fixed submit, added loading image.

---
+++ application/logs/2011/06/20.php
@@ -0,0 +1,4 @@
+<?php defined('SYSPATH') or die('No direct script access.'); ?>
+
+2011-06-20 01:10:28 --- ERROR: ErrorException [ 1 ]: Call to a member function render() on a non-object ~ SYSPATH/classes/kohana/controller/template.php [ 44 ]
+2011-06-20 01:19:26 --- ERROR: ErrorException [ 1 ]: Class 'Databse' not found ~ APPPATH/classes/controller/hardcore.php [ 55 ]
--- application/views/hardcore_enemy_submit.php
+++ application/views/hardcore_enemy_submit.php
@@ -5,26 +5,43 @@
 		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script>
 		<script type="text/javascript">
 			$(document).ready(function(
-
+				$("submit").click(function(){
+					$('#submit_monster').slideToggle(500);
+					$('#loading').slideToggle(500);
+					var query = $('submit_monster').serialize();
+					$.post('index.php/functions/submit', query, function(data, status){
+						
+					});
+					$('#loading').slideToggle(500);
+					$('#submit_monster').slideToggle(500);
+				});
 			));
 		</script>
 	</head>
-    <body style="background: url('/game/inc/images/concrete_wall.png');">
-		<div id="container">
-			<div id="header"></div>
-			<div id="content">
-				<form id="submit_monster" action="" style="background: #fff;">
-					<div style="display: block; width:100%; height: 20px; background: #c6c6c6;"></div>
-					<div class="fanwood" style="font-size: 20px;">Enemy Name</div> 
-					<input type="text" name="name"></input>
-					<div class="fanwood" style="font-size: 20px;">Manliness</div> 
-					<input type="text" name="manliness"></input>
-					<div class="fanwood" style="font-size: 20px;">Manliness Reward</div> 
-					<input type="text" name="manliness_reward"></input>
-					<div class="fanwood" style="font-size: 20px;">Money Reward</div> 
-					<input type="text" name="money_reward"></input>
-					<input type="hidden" name="author" value="<?$username?>"></input>
-				</form>
+    <body>
+    	<div style="float: left; position: relative; left: 50%;">
+			<div id="container">
+				<div id="header"></div>
+				<div id="content">
+					<div id="left" style="width: 100%;">
+						<div id="loading" display="hidden" style="display: block; background: #000; width: 100px; height: 100px; margin: 0 auto; margin-top: 100px; margin-bottom: 100px;">
+							<img src="/game/inc/images/loading.gif" />
+						</div>
+						<form id="submit_monster" action="" style="background: #ABABAB; margin: 0 auto; width: 50%; margin-top: 100px; margin-bottom:100px;">
+							<div style="display: block; width:100%; height: 50px; background: #666; font-size: 30pt; text-align: center; font-family: Arial;">Submit A Monster</div>
+							<div class="fanwood" style="font-size: 20px; margin-top: 20px; margin-left: 20px;">Enemy Name</div> 
+							<input type="text" name="name" style="margin-left: 20px"></input>
+							<div class="fanwood" style="font-size: 20px; margin-top: 20px; margin-left: 20px">Manliness</div> 
+							<input type="text" name="manliness" style="margin-left: 20px"></input>
+							<div class="fanwood" style="font-size: 20px; margin-top: 20px; margin-left: 20px">Manliness Reward</div> 
+							<input type="text" name="manliness_reward" style="margin-left: 20px"></input>
+							<div class="fanwood" style="font-size: 20px; margin-top: 20px; margin-left: 20px">Money Reward</div> 
+							<input type="text" name="richliness_reward" style="margin-left: 20px; margin-bottom: 20px;"></input>
+							<input type="hidden" name="author" value="<?$username?>"></input>
+							<div style="margin-left: 20px; margin-bottom: 20px;"><input id="submit" type="submit" name="submit" value="submit"></input></div>
+						</form>
+					</div>
+				</div>
 			</div>
 		</div>
 	</body>
